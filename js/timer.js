3523cf64c478607a0e270fbda0d4efed8dc77ce4
Sean Parker <sean.parker067@gmail.com>
2020-04-24 20:09:22 +0100

    Update timer to start of hackathon

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 // Configuration
-var startCountDownDate = new Date("Feb 06, 2020 9:00:00").getTime()
-var countDownDate = new Date("Apr 25, 2020 9:00:00").getTime();
+var startCountDownDate = new Date('Feb 06, 2020 9:00:00').getTime();
+var countDownDate = new Date('Apr 25, 2020 12:00:00').getTime();
 
 // Logic
 var currentTime = {
@@ -8,14 +8,14 @@ var currentTime = {
   hours: 0,
   minutes: 0,
   seconds: 0,
-}
+};
 
 var previousTime = {
   days: 0,
   hours: 0,
   minutes: 0,
   seconds: 0,
-}
+};
 
 $(document).queue(updateTime);
 
@@ -25,7 +25,9 @@ function updateTime() {
   var distance = countDownDate - now;
 
   currentTime.days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  currentTime.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  currentTime.hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
   currentTime.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   currentTime.seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -33,15 +35,18 @@ function updateTime() {
     if (previousTime[unit] == 0 || previousTime[unit] != currentTime[unit]) {
       previousTime[unit] = currentTime[unit];
     }
-    $("#timer-" + unit).html(currentTime[unit] < 10 ? "0" + currentTime[unit] : currentTime[unit]);
+    $('#timer-' + unit).html(
+      currentTime[unit] < 10 ? '0' + currentTime[unit] : currentTime[unit],
+    );
   }
-  const progressBarDist = ((now - startCountDownDate) / (countDownDate - startCountDownDate)) * 100;
-  $("#timer-progressbar").css("width", `${progressBarDist}%`);
+  const progressBarDist =
+    ((now - startCountDownDate) / (countDownDate - startCountDownDate)) * 100;
+  $('#timer-progressbar').css('width', `${progressBarDist}%`);
 
   if (distance < 0) {
     for (var unit in currentTime) {
-      $("#timer-" + unit).html("00");
-      $("#timer-progressbar").css("width", "100%");
+      $('#timer-' + unit).html('00');
+      $('#timer-progressbar').css('width', '100%');
     }
     return;
   }
